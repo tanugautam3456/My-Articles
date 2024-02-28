74e64d2956bf9bafab0829529f52db399dec6a82
tanu gautam <tanugautam3456@gmail.com>
2024-02-28 17:26:28 +0530

    login completed

--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -1,11 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Button, Label, TextInput,Alert, Spinner } from 'flowbite-react'
 
-function SignUp() {
+function signUp() {
+  const[loading,setLoading]=useState(false)
+  const[errorMessage,seterrorMessage]=useState(null)
+
+  const [formData, setFormData]= useState({})
+  const Navigate= useNavigate()
+
+  const handleChange = (e)=>{
+    setFormData({...formData,[e.target.id]: e.target.value.trim() })
+  }
+
+  // console.log(formData)
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+    if( !formData.email || !formData.password){
+      return seterrorMessage("All fields are required")
+    }
+    try {
+      seterrorMessage(null)
+      setLoading(true)
+      const res = await fetch('/api/auth/signin',{
+
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify(formData)
+  
+      })
+      // console.log(res)
+      const data = await res.json()
+      if(data.success ===false){
+        return seterrorMessage(data.message)
+      }
+      setLoading(false)
+
+      if(res.ok){
+        Navigate("/")
+      }
+
+    } catch (error) {
+      seterrorMessage(error.message)
+      setLoading(false)
+    }
+  }
   return (
-    <div>
-      signup
+    <div className='mt-20 min-h-screen'>
+
+      <div className='flex p-3 max-w-4xl mx-auto flex-col md:flex-row md:items-center gap-5' >
+
+        <div className='flex-1'>
+          <Link to='/' className='font-bold text-4xl dark:text-white'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Tanu's</span>Blog
+          </Link>
+          <p className='text-sm mt-5'>this is a demo project.you can signup with your email and password or with Google.</p>
+        </div>
+
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4'  onSubmit={handleSubmit}>
+            
+            
+            <div>
+              <Label value='Your Email'/>
+              <TextInput type='text' placeholder='Email' id='email' onChange={handleChange}/>
+            </div>
+            <div>
+              <Label value='Your Password'/>
+              <TextInput type='password' placeholder='Password' onChange={handleChange} id='password' />
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+              {loading ? (
+                <>
+                 <Spinner size='sm'/>
+                 <span className='pl-3'>loading</span>
+                </>
+              ):(
+                'Sign In'
+              )}
+            </Button>
+
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Don't Have an Account</span>
+            <Link to='/register' className='text-blue-500'>signUp</Link>
+          </div>
+          {errorMessage &&(
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
 
-export default SignUp
+export default signUp
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,14 +1,16 @@
 import User from '../Models/User.Models.js';
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 
 export const signup = async (req, res, next) => {
-    //  console.log(req.body)
+    console.log(req.body)
     const { username, email, password } = req.body
 
-    if (!username || !email || !password || username ==='' || email===''|| password==='') {
-
-        return res.status(400).json({ message: "All feilds are Requird" })
+    if (!username || !email || !password || username === '' || email === '' || password === '') {
+        return next(errorHandler(400, 'All fields are required '))
+        // return res.status(400).json({ message: "All feilds are Requird" })
     }
     // User.findOne({email})
     // .then(user=>{
@@ -16,14 +18,41 @@ export const signup = async (req, res, next) => {
     // })
     const hashedPassword = bcryptjs.hashSync(password, 10)
     const newUser = new User({
-        username, email, password :hashedPassword,
+        username, email, password: hashedPassword,
     })
     try {
         await newUser.save()
         res.json('User created')
-    } 
-    catch(error){
+    }
+    catch (error) {
         // res.status(500).json({message:error.message})
         next(error)
-    }  
+    }
+}
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body
+    if (!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are requuired'))
+    }
+    try {
+        const isValidUser = await User.findOne({ email })
+        if (!isValidUser) {
+            return next(errorHandler(401, 'Invalid Email or Password'))
+        }
+       const  isValidPassword = bcryptjs.compareSync(password, isValidUser.password)
+        if (!isValidPassword) {
+            return next(errorHandler(400, 'Invalid password'))
+        }
+        const token = jwt.sign({ id: isValidUser._id }, process.env.JWT_SECRET)
+        const { password: pass, ...others } = isValidUser._doc;
+
+        res.status(200)
+            .cookie('access_token', token, {
+                httpOnly: true
+            })
+            .json(others)
+    }
+    catch (error) {
+        next(error)
+    }
 }
