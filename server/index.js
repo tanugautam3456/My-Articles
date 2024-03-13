4457b3e0a69ddff18e0e6fa2b4564a13d77c318b
tanu gautam <tanugautam3456@gmail.com>
2024-03-13 18:07:54 +0530

    image upload completed

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@ import dotenv from 'dotenv'
 import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.route.js'
 
+import cookieParser from 'cookie-parser';
+
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express.Router();
-
+import { verifyToken } from '../utils/verifyUser.js';
+import {updateUser} from '../controllers/user.controller.js';
 router.get('/test',(req,res)=>{
     res.send("This is a test route");
 })
+router.put('/update/:userId', verifyToken, updateUser)
 export default router
